1954f3d6aefbd1cd154df3f723dd009c9f62f910
mun <git@t3hmun.com>
2019-05-28 07:46:39 +0100

    Minimal program.

---
+++ 002_compile_something/Program.cs
@@ -0,0 +1,12 @@
+namespace t3hmun.CompileSomthing
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            System.Console.WriteLine("Salutations");
+            System.Console.ReadKey();
+            return 0;
+        }
+    }
+}
